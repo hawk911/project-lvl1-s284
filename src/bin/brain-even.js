62e30590d22dfb0c2985eb9049b1b670696984f6
Denis B <hawk911@yandex.ru>
2018-05-30 10:12:43 +0200

    error correction

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import { brainEven } from '..';
 
-console.log('Welcome to Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
 brainEven();
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import readlineSync from 'readline-sync';
 
+const countQuestions = 3;
+
 export const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello ${userName}!\n`);
-  return userName;
 };
 
 const getRandomInt = (min, max) => Math.round(Math.random() * (max - min)) + min;
 const isEven = num => num % 2 === 0;
-const question = (num) => {
-  const answer = readlineSync.question(`Question: ${num}\n`);
-  console.log(`Your answer: ${answer}`);
-  return answer;
-};
 
 export const brainEven = () => {
-  const userName = askName();
-  const countQuestions = 3;
+  console.log('Welcome to Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!\n`);
+
   let countRightAnswersUser = 0;
   for (let i = 0; i < countQuestions; i += 1) {
     const randomNumber = getRandomInt(1, 50);
-    const answerUser = question(randomNumber);
+    const answerUser = readlineSync.question(`Question: ${randomNumber}\n`);
+    console.log(`Your answer: ${answerUser}`);
     const answerRight = isEven(randomNumber) ? 'yes' : 'no';
     if (answerUser === answerRight) {
       console.log('Correct!');
@@ -28,9 +28,10 @@ export const brainEven = () => {
     } else {
       console.log(`${answerUser} is wrong answer ;(. Correct answer was ${answerRight}.`);
       console.log(`Let's try again, ${userName}`);
+      break;
     }
-    if (countQuestions === countRightAnswersUser) {
-      console.log(`Congratulations, ${userName}!`);
-    }
+  }
+  if (countQuestions === countRightAnswersUser) {
+    console.log(`Congratulations, ${userName}!`);
   }
 };
